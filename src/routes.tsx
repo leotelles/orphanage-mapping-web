a83f4ffa9080afec27112afb316192cc7298dbf0
Leonardo Telles <leo.c.telles@gmail.com>
2020-11-02 14:41:04 -0300

    Create pages Orphanage e CreateOrphanage

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Landing from '../src/pages/Landing';
 import OrphanagesMap from '../src/pages/OrphanagesMap';
+import CreateOrphanage from '../src/pages/CreateOrphanage';
+import Orphanage from '../src/pages/Orphanage';
 
 function Routes() {
   return (
@@ -10,6 +12,9 @@ function Routes() {
       <Switch>
         <Route path="/" component={Landing} exact />
         <Route path="/app" component={OrphanagesMap} />
+        <Route path="/orphanages/create" component={CreateOrphanage} />
+        <Route path="/orphanages/:id" component={Orphanage} />
+
       </Switch>
     </BrowserRouter>
   );
